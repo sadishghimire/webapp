5e69d50caa139e94853733c2c77b93255a416967
sadish <sadish10.sg@gmail.com>
2025-08-08 11:53:44 +0545

    Git method is added

--- webapp/Program.cs
+++ webapp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -5,14 +7,64 @@ app.MapGet("/", () => "Hello World!");
 
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync($"The method is:{context.Request.Method}\r\n");
-    await context.Response.WriteAsync($"The url is:{context.Request.Path}\r\n");
-    await context.Response.WriteAsync($"The header is:{context.Request.Headers}\r\n");
-
-    foreach (var key in context.Request.Headers.Keys)
+    if (context.Request.Method == "GET")
     {
-        await context.Response.WriteAsync($"{key}:{context.Request.Headers[key]}\r\n");
+        if (context.Request.Path.StartsWithSegments("/")) 
+        {
+            //Endpoint handling
+            await context.Response.WriteAsync($"The method is:{context.Request.Method}\r\n");
+            await context.Response.WriteAsync($"The url is:{context.Request.Path}\r\n");
+            await context.Response.WriteAsync($"The header is:{context.Request.Headers}\r\n");
+
+            foreach (var key in context.Request.Headers.Keys)
+            {
+                await context.Response.WriteAsync($"{key}:{context.Request.Headers[key]}\r\n");
+            }
+        }
+
+        else if (context.Request.Path.StartsWithSegments("/employees"))
+        {
+            //Endpoint handling
+            var employees = Employeesrepository.GetEmployees();
+            foreach (var employee in employees) 
+            {
+                await context.Response.WriteAsync($"{employee.Name}:{employee.Address}\r\n");
+            }
+        }
+        
     }
+
+    
 });
 
 app.Run();
+
+static class Employeesrepository
+{
+    private static List<Employee> employees = new List<Employee>
+    {
+        new Employee(1,"sadish","kathmandu"),
+        new Employee(2,"Ram","palpa"),
+        new Employee(3,"Shyam","butwal")
+    };
+    public static List<Employee> GetEmployees() => employees;
+}
+
+
+public class Employee { 
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    
+    public Employee(int id, string name, string address)
+
+    {
+        Id = id;
+        Name=name; 
+        Address=address; 
+       
+    }
+
+}
+
