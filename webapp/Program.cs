fa562d8832b49984702eb2a54bf3e037ada58eee
sadish <sadish10.sg@gmail.com>
2025-08-08 11:28:22 +0545

    http request in httpcontext

--- webapp/Program.cs
+++ webapp/Program.cs
@@ -3,4 +3,16 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync($"The method is:{context.Request.Method}\r\n");
+    await context.Response.WriteAsync($"The url is:{context.Request.Path}\r\n");
+    await context.Response.WriteAsync($"The header is:{context.Request.Headers}\r\n");
+
+    foreach (var key in context.Request.Headers.Keys)
+    {
+        await context.Response.WriteAsync($"{key}:{context.Request.Headers[key]}\r\n");
+    }
+});
+
 app.Run();
